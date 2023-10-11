9809132f56eb48b3e46bff0a5ae815fd3af34933
DerPenz <mike2407@t-online.de>
2023-10-11 18:25:46 +0200

    added AND instruction

---
+++ src/main/java/org/penz/emulator/cpu/opcode/instructions/logical/ANDAInstruction.java
@@ -0,0 +1,27 @@
+package org.penz.emulator.cpu.opcode.instructions.logical;
+
+import org.penz.emulator.cpu.Alu;
+import org.penz.emulator.cpu.Registers;
+import org.penz.emulator.cpu.opcode.DataType;
+import org.penz.emulator.cpu.opcode.OpCode;
+import org.penz.emulator.memory.AddressSpace;
+
+/**
+ * AND accumulator with register A
+ */
+public class ANDAInstruction extends OpCode {
+
+    public ANDAInstruction() {
+        super(0xA7, "AND A", 4);
+    }
+
+    @Override
+    public int execute(Registers registers, AddressSpace addressSpace, Alu alu, int[] args) {
+
+        var aluOperation = alu.getOperation("AND", DataType.d8, DataType.d8);
+
+        registers.setA(aluOperation.apply(registers.getFlags(), registers.getA(), registers.getA()));
+
+        return cycles;
+    }
+}
---
+++ src/main/java/org/penz/emulator/cpu/opcode/instructions/logical/ANDEInstruction.java
@@ -0,0 +1,27 @@
+package org.penz.emulator.cpu.opcode.instructions.logical;
+
+import org.penz.emulator.cpu.Alu;
+import org.penz.emulator.cpu.Registers;
+import org.penz.emulator.cpu.opcode.DataType;
+import org.penz.emulator.cpu.opcode.OpCode;
+import org.penz.emulator.memory.AddressSpace;
+
+/**
+ * AND accumulator with register E
+ */
+public class ANDEInstruction extends OpCode {
+
+    public ANDEInstruction() {
+        super(0xA7, "AND E", 4);
+    }
+
+    @Override
+    public int execute(Registers registers, AddressSpace addressSpace, Alu alu, int[] args) {
+
+        var aluOperation = alu.getOperation("AND", DataType.d8, DataType.d8);
+
+        registers.setA(aluOperation.apply(registers.getFlags(), registers.getA(), registers.getE()));
+
+        return cycles;
+    }
+}
