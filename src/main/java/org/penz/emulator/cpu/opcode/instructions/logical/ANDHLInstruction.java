a376a7b91892a70a08f699c464d1f660635dbd7b
DerPenz <99838921+Der-Penz@users.noreply.github.com>
2023-10-18 13:27:18 +0200

    added missing HL AND A instruction

---
+++ src/main/java/org/penz/emulator/cpu/opcode/instructions/logical/ANDHLInstruction.java
@@ -0,0 +1,27 @@
+package org.penz.emulator.cpu.opcode.instructions.logical;
+
+import org.penz.emulator.cpu.Alu;
+import org.penz.emulator.cpu.Registers;
+import org.penz.emulator.cpu.opcode.DataType;
+import org.penz.emulator.cpu.opcode.OpCode;
+import org.penz.emulator.memory.AddressSpace;
+
+/**
+ * AND accumulator with the address pointed to by HL
+ */
+public class ANDHLInstruction extends OpCode {
+
+    public ANDHLInstruction() {
+        super(0xA6, "AND (HL)", 8);
+    }
+
+    @Override
+    public int execute(Registers registers, AddressSpace addressSpace, Alu alu, int[] args) {
+
+        var aluOperation = alu.getOperation("AND", DataType.d8, DataType.d8);
+
+        registers.setA(aluOperation.apply(registers.getFlags(), registers.getA(), addressSpace.readByte(registers.getHL())));
+
+        return cycles;
+    }
+}
